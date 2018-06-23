40128fa1eaf9a41e540c22e57d5392fc837360cc
alexTenFive <flash.alex322@gmail.com>
2018-06-23 15:52:22 +0300

    Register controller filled

--- app/Http/Controllers/ApiRegisterController.php
+++ app/Http/Controllers/ApiRegisterController.php
@@ -3,8 +3,38 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
+use Validator;
+use Response;
+use JWTAuth;
 
 class ApiRegisterController extends Controller
 {
-    //
+    /**
+     * User register function
+     */
+    public function register(Request $request) 
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|string|email|max:255|unique:users',
+            'name' => 'required',
+            'password' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors());
+        }
+
+        User::create([
+            'name' => $request->get('name'),
+            'email' => $request->get('email'),
+            'password' => bcrypt($request->get('password')),
+        ]);
+        
+        $user = User::first();
+        $token = JWTAuth::fromUser($user);
+
+        return Response::json(compact('token'));
+
+    }
 }
