e7052361edd8d14afee762ed859f654be750f4ef
alexTenFive <flash.alex322@gmail.com>
2018-06-23 15:40:22 +0300

    Add login and register controllers and routes for them

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,8 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+// JWT Auth routes
+Route::post('user/register', 'ApiRegisterController@register');
+Route::post('user/login', 'ApiLoginController@login');
